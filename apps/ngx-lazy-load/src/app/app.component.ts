a89eeec56649edf6e0d4d04d464da1e4c9a4bc36
Dimpu <buddhaaravind@gmail.com>
2019-07-16 15:29:29 -0400

    feat(lazy-load): create lazy load module

--- apps/ngx-lazy-load/src/app/app.component.ts
+++ apps/ngx-lazy-load/src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  ElementRef,
+  ComponentFactoryResolver,
+  Injector
+} from '@angular/core';
+import {LazyComponent} from './lazy/lazy.component';
 
 @Component({
   selector: 'ngx-lazy-load-root',
@@ -6,5 +13,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngx-lazy-load';
+  @ViewChild('viewRef', {static: false}) viewRef: ElementRef<HTMLElement>;
+  constructor(
+    public _compFact: ComponentFactoryResolver,
+    public injector: Injector
+  ) {}
+
+  loadLazy = async () => {
+    await import('./lazy/lazy.module').then(m => m.LazyModule);
+
+    const LazyModule = import('./lazy/lazy.module').then(m => m.LazyModule);
+    // create element
+    const lazy = document.createElement('ngx-lazy-load-demo-lazy');
+    // create factory for the componente
+    const factory = this._compFact.resolveComponentFactory(LazyComponent);
+
+    // create component reference
+    const LazyCompRef = factory.create(this.injector, [], lazy);
+    // LazyCompRef.instance.title = 'dimpu';
+
+    this.viewRef.nativeElement.appendChild(lazy);
+  };
 }
---
+++ apps/ngx-lazy-load/src/app/lazy/lazy.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-lazy-load-lazy',
+  template: `
+    <p>
+      lazy works!
+    </p>
+  `,
+  styles: []
+})
+export class LazyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
